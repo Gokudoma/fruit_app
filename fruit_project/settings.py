b8d16ce82640d65e1073d9b2687d54001f7cd632
Gokudoma <stephan_wacker@hotmail.com>
2025-10-20 05:50:33 +0200

    config: Add and configure django-cors-headers

--- fruit_project/settings.py
+++ fruit_project/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fruits',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Die Adresse, von der das Frontend senden wird
+CORS_ALLOWED_ORIGINS = [
+    "[http://127.0.0.1:5500](http://127.0.0.1:5500)", # Port vom VS Code Live Server
+]
