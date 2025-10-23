b158a98d5cfb9a624a0a2f49dc2bf1ddb9ee1cb2
Gokudoma <stephan_wacker@hotmail.com>
2025-10-23 11:41:49 +0200

    feat: Add new 'send_fruits' view to render HTML template

--- fruits/urls.py
+++ fruits/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('fruits/', views.fruit_list),
+    path('list/', views.send_fruits),
 ]
--- fruits/views.py
+++ fruits/views.py
@@ -2,14 +2,15 @@ from django.http import JsonResponse
 from .models import Fruit
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
 
 # Create your views here.
 
-@csrf_exempt # Nur für dieses einfache Beispiel, um POST-Anfragen zu erlauben
+@csrf_exempt
 def fruit_list(request):
     if request.method == 'GET':
         fruits = Fruit.objects.all()
-        data = list(fruits.values())  # Konvertiert die Objekte in eine Liste von Dictionaries
+        data = list(fruits.values())
         return JsonResponse(data, safe=False)
 
     elif request.method == 'POST':
@@ -21,3 +22,21 @@ def fruit_list(request):
             origin=data['origin']
         )
         return JsonResponse({'status': 'Fruit created!'}, status=201)
+
+
+# Daten aus der fruitsArrayBasic.txt
+fruits_static_list = [
+    {"name": "Apfel", "weight": 100, "color": "red"},
+    {"name": "Banane", "weight": 120, "color": "yellow"},
+    {"name": "Orange", "weight": 150, "color": "orange"},
+    {"name": "Birne", "weight": 130, "color": "green"},
+    {"name": "Kirsche", "weight": 10, "color": "red"}
+]
+
+
+def send_fruits(request):
+    context = {
+        'fruits': fruits_static_list
+    }
+    # Verknüpft mit fruitlist.html
+    return render(request, 'fruits/fruitlist.html', context)
